94f1e62209b0f0f755be900f74bfc3efc517634f
Gabriel Mai <gabriel.mai@belluno.digital>
2024-07-11 16:09:43 -0300

    feat: added test for invalid password error message in login

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -23,6 +23,17 @@ describe('Login', () => {
         cy.get('.woocommerce-error').should('exist')
     })
 
+        it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('gabriel.mai@teste.com')
+        cy.get('#password').type('Mudar@1')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail gabriel.mai@teste.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('exist')
+    })
+
+
 })
 
 
