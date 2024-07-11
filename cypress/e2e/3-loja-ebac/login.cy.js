6462d80102c448afedc194ce5e8103847ba59026
Gabriel Mai <gabriel.mai@belluno.digital>
2024-07-11 16:08:55 -0300

    test: added login error message test

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -13,6 +13,16 @@ describe('Login', () => {
 
     })
 
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('gabriel@teste.com')
+        cy.get('#password').type('Mudar@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error').should('exist')
+    })
+
 })
 
 
