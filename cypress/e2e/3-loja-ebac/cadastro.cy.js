520c3b1cda8cea535964f566bdad3571298b4176
Gabriel Mai <gabriel.mai@belluno.digital>
2024-07-14 21:07:31 -0300

    feat: improve user registration tests with preCadastro command

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -4,6 +4,13 @@ describe('Funcionalidade: Cadastro', () => {
 
     beforeEach(() => {
         cy.visit('minha-conta')
+        let user = {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            password: faker.internet.password()
+        }
+        user.email = faker.internet.email(user.firstName, user.lastName)
+        cy.wrap(user).as('user')
     })
 
     afterEach(() => {
@@ -26,26 +33,24 @@ describe('Funcionalidade: Cadastro', () => {
         cy.get(".woocommerce-message").should('contain', 'Detalhes da conta modificados com sucesso.')
     })
 
-        it('Deve completar o cadastro com sucesso - Usando variáveis', () => {
-            let user = {
-                firstName: faker.person.firstName(),
-                lastName: faker.person.lastName(),
-                password: faker.internet.password()
-            }
-            user.email = faker.internet.email(user.firstName)
-
-        cy.get("#reg_email").type(user.email)
-        cy.get("#reg_password").type(user.password)
-        cy.get("input[name='register']").click()
-
-        cy.get(".woocommerce-MyAccount-content > :nth-child(2)")
-            .should('exist')
-
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get("#account_first_name").type(user.firstName)
-        cy.get("#account_last_name").type(user.lastName)
+    it('Deve completar o cadastro com sucesso - Usando variáveis', () => {
+        cy.get('@user').then(user => {
+            cy.get("#reg_email").type(user.email)
+            cy.get("#reg_password").type(user.password)
+            cy.get("input[name='register']").click()
+            cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should('exist')
+            cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+            cy.get("#account_first_name").type(user.firstName)
+            cy.get("#account_last_name").type(user.lastName)
+        })
         cy.get(".woocommerce-Button").click()
+        cy.get(".woocommerce-message").should('contain', 'Detalhes da conta modificados com sucesso.')
+    })
 
+    it('Deve completar o cadastro com sucesso - Usando Comando Customizado', () => {
+        cy.get('@user').then(user => {
+            cy.preCadastro(user)
+        })
         cy.get(".woocommerce-message").should('contain', 'Detalhes da conta modificados com sucesso.')
     })
 
