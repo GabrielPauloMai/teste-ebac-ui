c89bc2e0ed37e72f72d8de6ceec053d119ea1b4d
Gabriel Mai <gabriel.mai@belluno.digital>
2024-08-03 15:10:18 -0300

    feat: Update Cypress configuration for project

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+   projectId: "s8sxqz",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
+    screenshotsOnRunFailure: true,
   },
 });
